3e8cc28c9406aac4ac0c8c1e4cfa86c4e9445775
Kaitlyn Rohm <128335120+kaitlynrohm@users.noreply.github.com>
2024-10-11 14:00:54 +1300

    Added dummy data and a way to get and add employees

--- pythonApi/firstApi/app.py
+++ pythonApi/firstApi/app.py
@@ -1,3 +1,4 @@
+# To start server: python app.py
 # Imports
 import json
 from flask import Flask, jsonify, request
@@ -5,9 +6,34 @@ from flask import Flask, jsonify, request
 # Create app
 app = Flask(__name__)
 
+# dummy data
+employees = [ { 'id': 1, 'name': 'Ashley' }, { 'id': 2, 'name': 'Kate' }, { 'id': 3, 'name': 'Joe' }]
+
 @app.route("/", methods=["GET"]) #Define route and method
 def root(): #Function for route
     return jsonify({"Message": "Root endpoint hit"})
+
+@app.route("/employees", methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+def employee_is_valid(employee):
+    for key in employee.keys():
+        if key != 'name':
+            return False
+        return True
+
+@app.route("/employees", methods=["POST"])
+def create_employee():
+    global nextEmployeeId
+    nextEmployeeId = len(employees) + 1
+    employee = json.loads(request.data)
+    if not employee_is_valid(employee):
+        return jsonify({"Error": "Invalid properties."}), 400
+    employee['id'] = nextEmployeeId
+    # nextEmployeeId += 1
+    employees.append(employee)
+    return { 'location': f'/employees/{employee["id"]}' }, 201
 
 # Run app
 if __name__ == "__main__":
