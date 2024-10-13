adbb00de78d96e6f0a8430fdce9655aa1b6d8a61
Kaitlyn Rohm <128335120+kaitlynrohm@users.noreply.github.com>
2024-10-14 08:58:17 +1300

    Set up moudles environment and venv, added gitignore

--- PythonApis/modules/app.py
+++ PythonApis/modules/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+# To start server: python app.py
+# Imports
+from flask import Flask, jsonify, request
 
+# Create app
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+@app.route("/", methods=["GET"])
+def root():
+    return jsonify({"Message": "Root endpoint hit"})
+
+# Run app
+if __name__ == "__main__":
+    app.run(port=5000)
